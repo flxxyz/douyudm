4b8cc5003dd19bf3c2042228441c022003054d3e
flxxyz <i@mdzz.name>
2020-06-18 05:32:53 +0800

    update: 调整一些内容

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ danmaku.stt = require('./stt')
 danmaku.util = require('./util')
 danmaku.logger = require('./logger')
 danmaku.Websocket = require('./websocket')
+danmaku.packet = require('./packet')
 
 module.exports = danmaku
