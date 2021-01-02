ccd13cf72ac03365cb3aa831d98c804f209f8694
flxxyz <i@mdzz.name>
2021-01-02 16:32:46 +0800

    modify: 更加准确的消息解析

--- src/stt.js
+++ src/stt.js
@@ -2,38 +2,41 @@ const util = require('./util')
 
 class STT {
     escape(v) {
-        return util.isUndefined(v) ? '' : v.toString().replace(/@/g, '@A').replace(/\//g, '@S')
+        return v.toString().replace(/@/g, '@A').replace(/\//g, '@S');
     }
 
     unescape(v) {
-        return util.isUndefined(v) ? '' : v.toString().replace(/@A/g, '@').replace(/@S/g, '/')
+        return v.toString().replace(/@S/g, '/').replace(/@A/g, '@');
     }
 
     serialize(raw) {
         if (util.isObject(raw)) {
-            return Object.keys(raw).map(k => `${k}@=${this.escape(this.serialize(raw[k]))}/`).join('')
+            return Object.entries(raw)
+                .map(([k, v]) => `${k}@=${this.serialize(v)}`)
+                .join('');
         } else if (Array.isArray(raw)) {
-            return raw.map(v => `${this.escape(this.serialize(v))}/`).join('')
-        } else if (util.isString(raw) || util.isNumber(raw)) {
-            return raw.toString()
+            return raw.map(v => `${this.serialize(v)}`).join('');
+        } else {
+            return this.escape(raw.toString()) + '/';
         }
     }
 
     deserialize(raw) {
         if (raw.includes('//')) {
-            return raw.split('//').filter(e => e !== '').map(item => this.deserialize(item))
+            return raw.split('//')
+                .filter(e => e !== '')
+                .map(item => this.deserialize(item));
         }
 
         if (raw.includes('@=')) {
-            return raw.split('/').filter(e => e !== '').reduce((o, s) => {
-                const [k, v] = s.split('@=')
-                o[k] = this.deserialize(this.unescape(v))
-                return o
-            }, {})
-        } else if (raw.includes('@A=')) {
-            return this.deserialize(this.unescape(raw))
+            return raw.split('/')
+                .filter(e => e !== '')
+                .reduce((o, s) => {
+                    const [k, v] = s.split('@=');
+                    return o[k] = this.deserialize(v), o;
+                }, {});
         } else {
-            return raw.toString()
+            return this.unescape(raw);
         }
     }
 }
